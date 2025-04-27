7b8afc57b2be46b68ad9a448cce79032e8d5fd95
ZAKI--V2.0.0 <zelghenam@gmail.com>
2025-04-27 21:57:51 +0000

    Create Hero section with profile, headline and action buttons

---
+++ src/components/Hero.tsx
@@ -0,0 +1,62 @@
+import { ButtonOutline, ButtonPrimary } from "./Button";
+import { HiOutlineDownload } from "react-icons/hi";
+import { IoArrowDown } from "react-icons/io5";
+
+const Hero = () => {
+  return (
+    <section className="pt-28 lg:pt-36" id="home">
+      <div className="container lg:grid lg:grid-cols-2 items-center lg:gap-10">
+        <div>
+          <div className="flex items-center gap-3">
+            <figure className="img-box w-9 h-9 rounded-lg">
+              <img
+                src="/images/avatar-1.jpg"
+                width={40}
+                height={40}
+                alt="Henry Clark portrait"
+                className="img-cover"
+              />
+            </figure>
+
+            <div className="flex items-center gap-1.5 text-zinc-400 text-sm tracking-wide">
+              <span className="relative w-2 h-2 rounded-full bg-emerald-400">
+                <span className="absolute inset-0 rounded-full bg-emerald-400 animate-ping"></span>
+              </span>
+              Available for work
+            </div>
+          </div>
+
+          <h2 className="headline-1 max-w-[15ch] sm:max-w-[20ch] lg:max-w-[15ch] mt-5 mb-8 lg:mb-10">
+            Building Scalable Modern Websites for the Future
+          </h2>
+
+          <div className="flex items-center gap-3">
+            <ButtonPrimary
+              label="Download CV"
+              icon={<HiOutlineDownload size={16} />}
+            />
+            <ButtonOutline
+              href="#about"
+              label="Scroll down"
+              icon={<IoArrowDown size={16} />}
+            />
+          </div>
+        </div>
+
+        <div className="hidden lg:block">
+          <figure className="w-full max-w-[480px] ml-auto bg-gradient-to-t from-sky-400 via-25% via-sky-400/40 to-65% rounded-[60px] overflow-hidden">
+            <img
+              src="./images/hero-banner.png"
+              width={656}
+              height={800}
+              alt="Hnery Clark"
+              className="w-full"
+            />
+          </figure>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
