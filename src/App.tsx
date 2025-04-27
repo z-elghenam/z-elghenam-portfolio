b7af4c8df9154ef92db10f13b94102baec04195b
ZAKI--V2.0.0 <zelghenam@gmail.com>
2025-04-27 22:12:53 +0000

    integrate Hero, About and Skill into App

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,17 @@
  * Components
  */
 import Header from "./components/Header";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Skill from "./components/Skill";
 
 function App() {
   return (
     <>
       <Header />
+      <Hero />
+      <About />
+      <Skill />
     </>
   );
 }
