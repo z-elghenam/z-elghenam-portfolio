1f063078edffe0a6942eeac6f9caa27bad570048
ZAKI--V2.0.0 <zelghenam@gmail.com>
2025-04-28 22:31:32 +0000

    remove reval-up class

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -43,7 +43,7 @@ export default function Work() {
   return (
     <section id="work" className="section">
       <div className="container">
-        <h2 className="headline-2 mb-8 reveal-up">My portfolio highlights</h2>
+        <h2 className="headline-2 mb-8">My portfolio highlights</h2>
 
         <div className="grid gap-x-4 gap-y-5 grid-cols-[repeat(auto-fill,_minmax(280px,_1fr))]">
           {works.map(({ imgSrc, title, tags, projectLink }, key) => (
