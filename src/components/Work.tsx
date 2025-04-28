7f2d8aa2a9ee0c8edcb9f52711ef16dd6d2a9a37
ZAKI--V2.0.0 <zelghenam@gmail.com>
2025-04-28 00:30:02 +0000

    feat: Integrate ProjectCard into Work section

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -1,3 +1,5 @@
+import ProjectCard from "./ProjectCard";
+
 const works = [
   {
     imgSrc: "/images/project-1.jpg",
@@ -38,5 +40,24 @@ const works = [
 ];
 
 export default function Work() {
-  return <div></div>;
+  return (
+    <section id="work" className="section">
+      <div className="container">
+        <h2 className="headline-2 mb-8 reveal-up">My portfolio highlights</h2>
+
+        <div className="grid gap-x-4 gap-y-5 grid-cols-[repeat(auto-fill,_minmax(280px,_1fr))]">
+          {works.map(({ imgSrc, title, tags, projectLink }, key) => (
+            <ProjectCard
+              key={key}
+              imgSrc={imgSrc}
+              title={title}
+              tags={tags}
+              projectLink={projectLink}
+              classes="reveal-up"
+            />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 }
